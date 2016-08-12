de5cd7a6a748a020a5723533fbe871413b80c11a
itaishin <itai99@icloud.com>
2016-08-12 14:01:36 +0300

    Initial commit

---
+++ MCTest/MCTest/NicknameViewController.swift
@@ -0,0 +1,63 @@
+//
+//  NicknameViewController.swift
+//  MCTest
+//
+//  Created by Itai Steinherz on 20/07/2016.
+//  Copyright © 2016 StarShell. All rights reserved.
+//
+
+import UIKit
+
+class NicknameViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if textField.text?.characters.count > 0 {
+            nextButton.enabled = true
+        } else {
+            nextButton.enabled = false
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        performSegueWithIdentifier("NicknameSegue", sender: self)
+        return true
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "NicknameSegue" {
+            if let dest = segue.destinationViewController as? ViewController {
+                dest.nickname = nicknameTextField.text!
+                dest.hadAskedForNickname = true
+            }
+        }
+    }
+    
+
+}
